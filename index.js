d54f6fc5363228f9a09bff012f8e96b74ea16ec9
Naveen Krishna P M <127135944+naveenkrishna4@users.noreply.github.com>
2024-06-02 19:29:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.post('/verify-recaptcha', async (req, res) => {
 app.use(express.static(path.join(__dirname, "frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "src/index.js"));
+  res.sendFile(path.join(__dirname, "frontend", "build", "public/index.html"));
 });
 
 // Start the server
